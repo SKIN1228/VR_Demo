43c98678c005f16fd77a355a2ecff5a455754167
SKIN1228 <s1620629@u.tsukuba.ac.jp>
2017-06-06 16:47:47 +0900

    hello_github

--- Assets/cube.cs
+++ Assets/cube.cs
@@ -6,11 +6,11 @@ public class cube : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log("Hello Github!");
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
